a72c4301605f807c86a376a6caebfe9fb52e325f
risonakamo <hmykanae@gmail.com>
2018-03-19 17:35:31 -0400

    did the css-ing

--- betterrandomorg.js
+++ betterrandomorg.js
@@ -6,11 +6,8 @@ homepageGen.children[0].onload=()=>{
     homepageGen.children[0].contentDocument.head.insertAdjacentHTML("beforeend",
     `<link rel="stylesheet" href="${chrome.runtime.getURL("betterrandomorg2.css")}">`);
 
-    // var mainElement=homepageGen.children[0].contentDocument
-    //     .querySelector("#true-random-integer-generator");
+    var mainElement=homepageGen.children[0].contentDocument
+        .querySelector("#true-random-integer-generator");
 
-    // mainElement.style.border="none";
-    // mainElement.removeChild(mainElement.children[6]);
-    // mainElement.removeChild(mainElement.children[4]);
-    // mainElement.removeChild(mainElement.children[0]);
+    mainElement.children[1].children[1].focus();
 };
